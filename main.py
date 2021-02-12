853c56cda49851e4d5e88446f81b833d3eae210c
Azeem <livelyazeem@gmail.com>
2021-02-12 11:13:30 +0500

    w

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 #chapter: databases
-
+'''
 import sqlite3
 conn = sqlite3.connect('emaildb.sqlite')
 cur = conn.cursor()
 
 cur.execute('DROP TABLE IF EXISTS Counts')
 
-cur.execute('''
-CREATE TABLE Counts (email TEXT, count INTEGER)''')
+cur.execute('CREATE TABLE Counts (email TEXT, count INTEGER)')
 
 fname = input('Enter file name: ')
 if (len(fname) < 1): fname = 'mbox-short.txt'
@@ -19,8 +18,7 @@ for line in fh:
     cur.execute('SELECT count FROM Counts WHERE email = ?', (email,))
     row = cur.fetchone()
     if row is None:
-        cur.execute('''INSERT INTO Counts (email, count)
-                VALUES (?, 1)''', (email,))
+        cur.execute('INSERT INTO Counts (email, count) VALUES (?, 1)', (email,))
     else:
         cur.execute('UPDATE Counts SET count = count + 1 WHERE email = ?', (email,))
     conn.commit()
@@ -32,3 +30,5 @@ for row in cur.execute(sqlstr):
     print(str(row[0]), row[1])
 
 cur.close()
+'''
+
