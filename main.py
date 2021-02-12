ff1e03a7baef19c60dc697e5bf4c24d7a18b833b
Azeem <livelyazeem@gmail.com>
2021-02-12 19:11:04 +0500

    h

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #chapter: databases
-
+'''
 import sqlite3
 
 conn = sqlite3.connect('emaildb.sqlite')
@@ -33,3 +33,61 @@ for row in cur.execute(sqlstr):
     print(str(row[0]), row[1])
 
 cur.close()
+'''
+
+
+from urllib.request import urlopen
+import urllib.error
+import twurl
+import json
+import sqlite3
+import ssl
+
+TWITTER_URL = 'https://api.twitter.com/1.1/friends/list.json'
+
+conn = sqlite3.connect('spider.sqlite')
+cur = conn.cursor()
+
+cur.execute('''
+            CREATE TABLE IF NOT EXISTS Twitter
+            (name TEXT, retrieved INTEGER, friends INTEGER)''')
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+while True:
+    acct = input('Enter a Twitter account, or quit: ')
+    if (acct == 'quit'): break
+    if (len(acct) < 1):
+        cur.execute('SELECT name FROM Twitter WHERE retrieved = 0 LIMIT 1')
+        try:
+            acct = cur.fetchone()[0]
+        except:
+            print('No unretrieved Twitter accounts found')
+            continue
+
+    url = twurl.augment(TWITTER_URL, {'screen_name': acct, 'count': '5'})
+    print('Retrieving', url)
+    coection = urlopen(url, context=ctx)
+    data = connection.read().decode()
+    headers = dict(connection.getheaders())
+
+    print('Remaining', headers['x-rate-limit-remaining'])
+    js = json.loads(data)
+    # Debugging
+    # print json.dumps(js, indent=4)
+
+    cur.execute('UPDATE Twitter SET retrieved=1 WHERE nanme = ?', (acct, ))
+
+    countnew = 0
+    countold = 0
+    for u in js['users']:
+        friend = u['screen_name']
+        print(friend)
+        cur.execute('SELECT friends FROM Twitter WHERE name = ? LIMIT 1', (friend, ))
+
+        try:
+            count = cur.fetchone()[0]
+            
